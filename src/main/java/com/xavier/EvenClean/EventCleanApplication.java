14cd69b0a17bca02cc8c745bac31d24ac466c561
Anthony Xavier <anthonyltx12@gmail.com>
2026-01-22 16:52:49 -0300

    correção do nome

--- src/main/java/com/xavier/EvenClean/EventCleanApplication.java
+++ src/main/java/com/xavier/EvenClean/EventCleanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EvenCleanApplication {
+public class EventCleanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EvenCleanApplication.class, args);
+		SpringApplication.run(EventCleanApplication.class, args);
 	}
 
 }
